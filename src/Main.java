4cdc4bc6794112dd962d6e35587f3ee545bd8126
Khoi Pham <khoi171195@gmail.com>
2019-01-25 08:22:51 +0700

    firstcommit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+<ul>
+<li><h2>Họ và tên</h2></li>
+<li><h2>Địa chỉ</h2></li>
+<li><h2>Số điện thoại</h2></li>
+<li><h2>Email</h2></li>
+<li><h2>Ảnh</h2></li>
+<li><h2>Học vấn</h2></li>
+<li><h2>Bằng cấp</h2></li>
+<li><h2>Kỹ năng</h2></li>
+<li><h2>Kinh nghiệm</h2></li>
+<li><h2>Định hướng phát triển cá nhân</h2></li>
+</ul>
